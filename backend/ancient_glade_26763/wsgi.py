d47bf0803fc1b80c95212b131f98ed4bd7b6c575
Crowdbotics <team@crowdbotics.com>
2021-05-16 23:37:15 +0000

    Initial commit.

---
+++ backend/ancient_glade_26763/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ancient_glade_26763 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ancient_glade_26763.settings')
+
+application = get_wsgi_application()
